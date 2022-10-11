ea76134c23841dcfe961770dbef52e7aa642d359
yudjii <114521187+yudjii@users.noreply.github.com>
2022-10-11 21:55:02 +0200

    space invader progress

--- spaceInvader.js
+++ spaceInvader.js
@@ -12,26 +12,42 @@ function spaceinvader() {
 
    context.fillStyle = "black";
    context.fillRect(300, 300, 300, 300);
-   
+
    context.fillStyle = "#66FF66";
-   context.fillRect(460,536,50,50);
-   context.fillRect(400,536,50,50);
-   context.fillRect(350,400,50,50);
-   context.fillRect(500,400,50,50);
-   context.fillRect(510,400,50,50);
-   context.fillRect(340,400,50,50);
-   context.fillRect(340,300,50,50);
-   context.fillRect(399,300,50,50);
-   context.fillRect(459,300,50,50);
-   context.fillRect(520,300,50,50);
 
-   context.fillRect(509,487,50,50);
-   context.fillRect(459,487,50,50);
-   context.fillRect(409,487,50,50);
-   context.fillRect(359,487,50,50);
+   //LEFT LINE//
 
-   context.fillRect(510,410,50,50);
-   context.fillRect(340,410,50,50);
+   context.fillRect(325,526,50,50);
+   
+   context.fillRect(325,476,50,50);
+
+   context.fillRect(325,426,50,50);
+
+   context.fillRect(325,376,50,50);
+
+
+   context.fillRect(325,326,50,50);
+
+
+   //RIGHT LINE//
+
+   context.fillRect(520,326,50,50);
+
+   context.fillRect(520,470,50,50);
+
+   context.fillRect(520,370,50,50);
+
+   context.fillRect(520,420,50,50);
+
+   context.fillRect(520,520,50,50);
+
+   context.fillRect(520,527,50,50);
+
+
+   //MID DOTS//
+
+
+
 
 
 }
